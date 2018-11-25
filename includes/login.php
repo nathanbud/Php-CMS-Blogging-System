9c26ce88b836824b688d67cb3ae621cef76a5c99
nathanbud <nthnlbdky916@gmail.com>
2018-11-25 23:10:49 +0800

    Added session to user's login

--- includes/login.php
+++ includes/login.php
@@ -1,4 +1,5 @@
 <?php include "db.php"; ?>
+<?php session_start(); ?>
 
 <?php
 
@@ -29,6 +30,12 @@ if(isset($_POST['login'])){
   if($username !==  $db_username && $password !== $db_user_password ){
     header("Location: ../index.php");
   }else if($username ==  $db_username && $password == $db_user_password){
+    
+    $_SESSION['username'] = $db_username;
+    $_SESSION['firstname'] = $db_user_firstname;
+    $_SESSION['lastname'] = $db_user_lastname_;
+    $_SESSION['user_role'] = $db_user_role;
+
     header("Location: ../admin");
   }
   else{
